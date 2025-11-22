653ca5048c31141f63ce507a428da698512ad2dc
tinchomorilla <mmorilla@fi.uba.ar>
2025-11-22 13:03:12 -0300

    delete files

--- bbe_sdk/utils/logger.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-
-def initialize_logging(logging_level: str):
-    logging.basicConfig(
-        level=getattr(logging, logging_level),
-        format="%(asctime)s - %(levelname)s - %(message)s",
-    )
