18eb4a5da0a79704fd503f0efd310d21d6cf6192
jfgulden <guldenjf@gmail.com>
2025-12-16 15:14:07 -0300

    add logs function to init

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,3 @@
-from .session import BlackBoxSession
+from .session import BlackBoxSession, configure_default_logging
 
-__all__ = ["BlackBoxSession"]
+__all__ = ["BlackBoxSession", "configure_default_logging"]
