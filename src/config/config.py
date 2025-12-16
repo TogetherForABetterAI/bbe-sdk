648dde3fd3e62eb058cbde905fbda56ca366ee13
iinaki <llorensinaki22@gmail.com>
2025-12-16 14:58:59 -0300

    chore: Modify CONNECTION_SERVICE_BASE_URL

--- src/config/config.py
+++ src/config/config.py
@@ -1,7 +1,8 @@
 import os
 
 
-CONNECTION_SERVICE_BASE_URL = "https://app.136.114.87.151.nip.io"
+#CONNECTION_SERVICE_BASE_URL = "https://app.136.114.87.151.nip.io"
+CONNECTION_SERVICE_BASE_URL = "http://localhost:8080"
 DISPATCHER_QUEUE = "%s_dispatcher_queue"
 CALIBRATON_QUEUE = "%s_outputs_cal_queue"
 
